845a1d97d49fdc4eb633b32ade2934c4fa12f75b
Sam Rabeeh <srabeeh@larci.com>
2016-09-19 10:10:37 +0700

    Initial Commit

---
+++ MapkitPrototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapkitPrototype
+//
+//  Created by Sam Rabeeh on 2016-09-19.
+//  Copyright © 2016 Sam Rabeeh - RCI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
